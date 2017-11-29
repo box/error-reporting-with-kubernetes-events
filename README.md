51db66345e2ec927dad3559616d676ddacd1b35e
Hakan Baba <hakanbaba83@gmail.com>
2017-11-28 21:51:57 -0800

    Complete example

--- README.md
+++ README.md
@@ -1,2 +1,188 @@
 # error-reporting-with-kubernetes-events
-Example apps running on kubernetes utilizing kubernetes events.
+
+[![Project Status](http://opensource.box.com/badges/stable.svg)](http://opensource.box.com/badges)
+
+Example containers running on kubernetes utilizing kubernetes events.
+
+There are two families of containers, from control plane and application level.
+The application level containers, can trigger additional processing in the
+control plane container. CustomResourceDefinitions are used for triggering the
+processing and passing parameters to the computation. The control plane
+container does some checking of the passed parameters. If there is any
+unexpected values in the parameters, [*Kubernetes
+Events*](https://v1-7.docs.kubernetes.io/docs/api-reference/v1.7/#event-v1-core)
+are used to relay the error information back to the application container.
+
+
+# Prerequisites
+
+Even though the code *should* work in later versions, this example is tested
+with kubernetes 1.7.5. More specifically:
+
+```
+$ kubectl version
+Client Version: version.Info{Major:"1", Minor:"7", GitVersion:"v1.7.5", GitCommit:"17d7182a7ccbb167074be7a87f0a68bd00d58d97", GitTreeState:"clean", BuildDate:"2017-08-31T19:32:12Z", GoVersion:"go1.9", Compiler:"gc", Platform:"darwin/amd64"}
+Server Version: version.Info{Major:"1", Minor:"7", GitVersion:"v1.7.5", GitCommit:"17d7182a7ccbb167074be7a87f0a68bd00d58d97", GitTreeState:"clean", BuildDate:"2017-10-04T09:07:46Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"linux/amd64"}
+```
+
+> NOTE: Because of the lack of availability of
+> [CustomResourceDefinitions](https://kubernetes.io/docs/tasks/access-kubernetes-api/extend-api-custom-resource-definitions/),
+> this example will not work with kubernetes versions earlier than 1.7.
+
+
+On a MacOs using
+[minikube](https://kubernetes.io/docs/getting-started-guides/minikube/#specifying-the-kubernetes-version) you can
+start a kubernetes server at v1.7.5.
+```
+minikube start --kubernetes-version v1.7.5
+```
+[brew](https://brew.sh/) can be used to install an old kubectl at
+[v1.7.5](https://github.com/Homebrew/homebrew-core/blob/8e0e4c9c9b1c4154f31f3313e6b5cfce7de79109/Formula/kubernetes-cli.rb#L5).
+
+You should use the docker daemon in the minikube vm. Follow the instructions
+[here](https://kubernetes.io/docs/getting-started-guides/minikube/#reusing-the-docker-daemon)
+
+Make sure you are using a recent version of docker. This is tested using these
+versions
+
+```
+$ docker version
+Client:
+ Version:      17.11.0-ce
+ API version:  1.23
+ Go version:   go1.9.2
+ Git commit:   1caf76c
+ Built:        unknown-buildtime
+ OS/Arch:      darwin/amd64
+
+Server:
+ Version:      17.06.0-ce
+ API version:  1.30 (minimum version 1.12)
+ Go version:   go1.8.3
+ Git commit:   02c1d87
+ Built:        Fri Jun 23 21:51:55 2017
+ OS/Arch:      linux/amd64
+ Experimental: false
+```
+
+> NOTE: For older versions of docker, we have seen problems with the `COPY`
+> command in Dockerfiles while copying a large number of files.
+
+# Build
+
+## Build the docker containers
+
+From project root execute:
+
+```
+docker build  -f cmd/controlplane/Dockerfile \
+   -t boxinc/error-reporting-with-kubernetes-events:controlplane  .
+```
+
+# Run
+
+## Start the control plane app and define the CustomResourceDefinition
+
+From repo root:
+```
+kubectl  apply  -f cmd/controlplane/config.yml
+```
+
+Expected output:
+```
+customresourcedefinition "pkis.box.com" configured
+namespace "controlplane" configured
+deployment "controlplane" configured
+```
+
+## Start the applications that trigger processing in the control plane app
+
+
+From repo root:
+```
+kubectl apply  -f cmd/app1/config.yml
+kubectl apply  -f cmd/app2/config.yml
+```
+
+Expected output:
+
+```
+namespace "app1" configured
+pki "app1-pki" configured
+deployment "app1" configured
+
+namespace "app2" configured
+pki "app2-pki" configured
+deployment "app2" created
+```
+
+
+# Expected output
+
+All [pods](https://kubernetes.io/docs/concepts/workloads/pods/pod/) except the
+pod for `app1` should transition to `Running` state eventually.
+
+```
+$ kubectl get pods --all-namespaces
+NAMESPACE      NAME                            READY     STATUS              RESTARTS   AGE
+app1           app1-647877271-lrpbd            0/1       ContainerCreating   0          1d
+app2           app2-3513338968-9hf6l           1/1       Running             1          1d
+controlplane   controlplane-1148696967-13dgv   1/1       Running             0          1d
+....
+```
+
+There is an error in `app1`'s `config.yml` to demonstrate the error
+handling using [Kubernetes
+Events](https://v1-7.docs.kubernetes.io/docs/api-reference/v1.7/#event-v1-core)
+
+The events history in `kubectl describe` output provides diagnostic
+information to the application owner so that she can fix the error
+easily.
+
+```
+kubectl describe pod app1-647877271-lrpbd --namespace app1
+
+Events:
+  FirstSeen   LastSeen   Count   From         SubObjectPath   Type      Reason         Message
+  ---------   --------   -----   ----         -------------   --------   ------         -------
+  ....
+  1d      1m      24   controlplane            Warning      pki ServiceName error   ServiceName: appp1 in pki: app1-pki is not found in allowedNames: [app1 app2]
+  ....
+
+```
+
+The shown error line is generated by the controlplane app and placed at the
+event stream of the problematic pod. For more details about generating this
+error event, can be found at the controlplane application's implementation.
+
+The application programmer can easily rootcause that the `serviceName` field
+in `config.yml` has a typo.
+```
+spec:
+  # This service name has a typo
+  serviceName: appp1
+```
+
+
+
+
+
+## Support
+
+Need to contact us directly? Email oss@box.com and be sure to include the name of this project in the subject.
+
+## Copyright and License
+
+Copyright 2017 Box, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ cmd/controlplane/main.go
@@ -0,0 +1,237 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	pkiV1 "github.com/box/error-reporting-with-kubernetes-events/pkg/apis/box.com/v1"
+	"github.com/golang/glog"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/tools/reference"
+)
+
+const allowedNamesFilePath string = "/AllowedNames"
+
+// allowedNames returns the allowed serviceName values in PKi objects.
+// The file in allowedNamesFilePath is read and its contents are retuned in a
+// slide. One line each slice entry.
+func allowedNames() []string {
+	file, err := os.Open(allowedNamesFilePath)
+	if err != nil {
+		glog.Fatalf("Could not open file: %s", allowedNamesFilePath)
+	}
+	defer file.Close()
+
+	rv := []string{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) != 0 {
+			rv = append(rv, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		glog.Fatal(err)
+	}
+
+	return rv
+}
+
+// isInSlice returns true if the string e exists in slice s
+func isInSlice(s []string, e string) bool {
+	for _, x := range s {
+		if x == e {
+			return true
+		}
+	}
+	return false
+}
+
+// watchPkis uses the http watch endpoint to "react" to changes to the
+// pkis. A goroutine with an infinite loop is started that continously
+// waits on the http watch endpoint. The errors are logged, the received pki's
+// are insered into the channel that is returned.
+func watchPkis(restClient restclient.Interface) <-chan pkiV1.PkiEvent {
+
+	pkisWatchPath := "/apis/box.com/v1/pkis"
+
+	events := make(chan pkiV1.PkiEvent)
+	go func() {
+		for {
+			// TODO: Instead of this direct HTTP request, the workqueue and
+			// informer pattern could be used. Similar to here
+			// https://github.com/kubernetes/sample-controller/blob/master/controller.go#L122
+			request := restClient.Get().AbsPath(pkisWatchPath).Param("watch", "true")
+			glog.Infof("HTTP Get request for: %s", request.URL())
+			body, err := request.Stream()
+			if err != nil {
+				glog.Errorf("restClient Stream Error  %v.", err)
+				continue
+			}
+			defer body.Close()
+
+			decoder := json.NewDecoder(body)
+
+			// Incrementally parse the body as long as it grows.
+			for {
+				var event pkiV1.PkiEvent
+				err = decoder.Decode(&event)
+				if err != nil {
+					glog.Errorf("Json decode error %v.", err)
+					break
+				}
+				events <- event
+			}
+			glog.Infof("Completed one Pki reception and decode")
+		}
+	}()
+	return events
+}
+
+// doPkiProcessing is a placeholder function for the business logic in this
+// control plane application. Once a valid Pki object is found, additional
+// processing would be triggered from this function. At the end the output is
+// written back to the api server in a Secret so that the interested parties
+// can mount it.
+func doPkiProcessing(pki pkiV1.Pki, kubeClient *kubernetes.Clientset) {
+	//
+	// .....
+	// Omitted extra processing for brevity
+	//
+
+	// Generate a Secret with the processing result and write back to the
+	// api server. The application does a secret mount and retrieves this
+	// data.
+
+	// A placeholder empty secret. In real life, the data in this
+	// secret would contain the generated pki information.
+	emptySecretSpec := v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      pki.Name,
+			Namespace: pki.Namespace,
+		},
+	}
+
+	// Replace the Pki
+	err := kubeClient.CoreV1().Secrets(pki.Namespace).Delete(pki.Name,
+		&metav1.DeleteOptions{})
+	if err != nil {
+		glog.Infof("Could not delete Secret with name %s: %v\n", pki.Name, err)
+	}
+
+	_, err = kubeClient.CoreV1().Secrets(pki.Namespace).Create(
+		&emptySecretSpec)
+	if err != nil {
+		glog.Fatalf("Could not generate Secret: %v", err)
+	}
+}
+
+func postEventAboutPki(pki pkiV1.Pki, kubeClient *kubernetes.Clientset,
+	recorder record.EventRecorder, allowedNames []string) error {
+
+	pods, err := kubeClient.CoreV1().Pods(pki.Namespace).List(metav1.ListOptions{})
+	if err != nil {
+		glog.Fatalf(" Could not list pods in namespace %s: %v", pki.Namespace, err)
+	}
+	for _, pod := range pods.Items {
+		// TODO: Even in the same namespace, there may be some pods that do not use
+		// this pki. Ideally we only want to send this message to the relevant pods.
+		// One needs to inspect the volumeMounts done by the containers in pods.
+		// If a container does a volumeMount with the same name, only then post this
+		// error message to that Pod's lifecycle. This is left as an excercise for the
+		// reader.
+
+		ref, err := reference.GetReference(scheme.Scheme, &pod)
+		if err != nil {
+			glog.Fatalf("Could not get refecence for pod %v: %v\n", pod.Name, err)
+		}
+		recorder.Event(ref, v1.EventTypeWarning, "pki ServiceName error",
+			fmt.Sprintf("ServiceName: %s in pki: %s is not found in allowedNames: %s",
+				pki.Spec.ServiceName, pki.Name, allowedNames))
+	}
+	return nil
+}
+
+// handlePkiEvents waits for new Pki's to be added or existing ones to be modified.
+// After it such event, it does parameter checking and some external processing.
+func handlePkiEvents(pkiEvents <-chan pkiV1.PkiEvent, allowedNames []string,
+	kubeClient *kubernetes.Clientset, recorder record.EventRecorder) {
+
+	for e := range pkiEvents {
+		glog.Infof("Seen PkiEvent : %v", e)
+		t := e.Type
+		pki := e.Object
+		if t == watch.Added || t == watch.Modified {
+			serviceName := pki.Spec.ServiceName
+			if isInSlice(allowedNames, serviceName) {
+				// The input parameters are ok. Do extra processing ...
+				doPkiProcessing(pki, kubeClient)
+			} else {
+				// Found some unexpected parameters in the pki. Generate an error
+				// message using kubernetes events.
+				postEventAboutPki(pki, kubeClient, recorder, allowedNames)
+			}
+		} else {
+			// There are other types of changes, Deleted, Modified etc that we do
+			// not care in this example.
+			glog.Infof("Received an unhandled pki change: %s for %v. Ignoring!", t, pki)
+		}
+		glog.Flush()
+	}
+}
+
+// var (
+// SchemeBuilder runtime.SchemeBuilder
+// )
+
+// eventRecorder returns an EventRecorder type that can be used to post Events
+// to different object's lifecycles.
+func eventRecorder(kubeClient *kubernetes.Clientset) (record.EventRecorder, error) {
+	// SchemeBuilder.AddToScheme(scheme.Scheme)
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(glog.Infof)
+	eventBroadcaster.StartRecordingToSink(
+		&typedcorev1.EventSinkImpl{Interface: kubeClient.CoreV1().Events("")})
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{
+		Component: "controlplane"})
+	return recorder, nil
+}
+
+func main() {
+
+	flag.Parse()
+
+	allowedNames := allowedNames()
+
+	kubeConfig, err := restclient.InClusterConfig()
+	if err != nil {
+		glog.Fatalf("Could not get kubeconfig: %v", err)
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		glog.Fatalf("Error building kubernetes clientset: %v", err)
+	}
+
+	restClient := kubeClient.RESTClient()
+	recorder, err := eventRecorder(kubeClient)
+	if err != nil {
+		glog.Fatalf("Error getting EventRecorder: %v", err)
+	}
+	pkiEvents := watchPkis(restClient)
+	handlePkiEvents(pkiEvents, allowedNames, kubeClient, recorder)
+}
