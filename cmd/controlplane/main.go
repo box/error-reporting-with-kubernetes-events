e3f4f120efb44b00d5a2ca0a1ad8b7e1f5a24417
Hakan Baba <hakanbaba83@gmail.com>
2017-12-10 15:01:23 -0800

    Add the kube api client accessors

--- cmd/controlplane/main.go
+++ cmd/controlplane/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/golang/glog"
 
 	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
 )
 
 const allowedNamesFilePath string = "/AllowedNames"
@@ -39,7 +40,62 @@ func allowedNames() []string {
 	return rv
 }
 
+// // watchPkis uses the http watch endpoint to "react" to changes to the
+// // pkis. A goroutine with an infinite loop is started that continously
+// // waits on the http watch endpoint. The errors are logged, the received pki's
+// // are insered into the channel that is returned.
+// func (r *thirdPartyResourceReader) watchPkis() <-chan PkiEvent {
+
+// httpEndpoint := r.config.Host + r.pkisWatchEndpoint
+
+// events := make(chan PkiEvent)
+// go func() {
+// for {
+// resp, err := r.httpClient.Get(httpEndpoint)
+// if err != nil {
+// handleHttpGetError(err, r.retrySeconds)
+// continue
+// } else if resp.StatusCode != 200 {
+// handleHttpGetError(errors.New("Invalid status code: "+resp.Status),
+// r.retrySeconds)
+// continue
+// }
+
+// decoder := json.NewDecoder(resp.Body)
+
+// // Incrementally parse the body as long as it grows, It looks like this
+// // is the behavior in watch endpoints
+// for {
+// var event PkiEvent
+// err = decoder.Decode(&event)
+// if err != nil {
+// handleJsonDecodeError(err)
+// break
+// }
+// events <- event
+// }
+// resp.Body.Close()
+
+// glog.Infof("Completed one Pki reception and decode")
+// }
+// }()
+// return events
+// }
+
 func main() {
-	var _ *kubernetes.Clientset
+
+	kubeConfig, err := restclient.InClusterConfig()
+	if err == nil {
+		glog.Fatalf("Could not get kubeconfig", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		glog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	restClient := kubeClient.RESTClient()
+	_ = restClient
+
 	fmt.Println("vim-go")
 }
--- pkg/apis/box.com/v1/types.go
+++ pkg/apis/box.com/v1/types.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 )
 
 // TODO: For a more complete example, this file could be the source of code
@@ -9,6 +10,15 @@ import (
 // https://github.com/kubernetes/sample-controller/blob/master/pkg/apis/samplecontroller/v1alpha1/types.go#L23)
 // However the purpose of this example is to demonstrate Kubernetes Events usage
 // for error messages. We have not used auto code generation for that.
+
+// PkiEvent is received as a resonse from the http watch endpoint.
+type PkiEvent struct {
+	// Type can take values like "ADDED" and "DELETED" to indicate the action
+	// causing the watch endpoint to notify.
+	Type watch.EventType `json:"type"`
+	// Object is the actual value type that has changed.
+	Object Pki `json:"object"`
+}
 
 // Pki is the specification of a pki.box.com resource
 type Pki struct {
